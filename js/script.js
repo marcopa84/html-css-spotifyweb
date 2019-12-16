ce03bb810c0ebd41b8f8591238feba4058cada78
Marco <marcopa84@hotmail.it>
2019-12-16 18:09:08 +0100

    second playlist + home link DONE!

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
-// // alert('hello!');
-// function myfunction(britney) {
-//   document.getElementById('title1').innerHTML='britney spears';
-//   document.getElementById('subtitle1').innerHTML='baby one more time';
-// }
-function myFunction() {
+function britney_spears() {
   document.getElementById('playlist_title_first_row').innerHTML='the best of britney spears';
   document.getElementById('first_row_image1').src='img/_britney1.jpg';
   document.getElementById('first_row_title1').innerHTML='britney spears';
@@ -23,4 +18,26 @@ function myFunction() {
   document.getElementById('first_row_image6').src='img/_britney6.jpg';
   document.getElementById('first_row_title6').innerHTML='britney spears';
   document.getElementById('first_row_subtitle6').innerHTML='work bitch!';
+}
+
+function italian_songs() {
+  document.getElementById('playlist_title_first_row').innerHTML='the best of italian songs';
+  document.getElementById('first_row_image1').src='img/_italian_songs_1.jpg';
+  document.getElementById('first_row_title1').innerHTML='mina';
+  document.getElementById('first_row_subtitle1').innerHTML='raso';
+  document.getElementById('first_row_image2').src='img/_italian_songs_2.jpg';
+  document.getElementById('first_row_title2').innerHTML='lucio battisti';
+  document.getElementById('first_row_subtitle2').innerHTML='emozioni';
+  document.getElementById('first_row_image3').src='img/_italian_songs_3.jpg';
+  document.getElementById('first_row_title3').innerHTML='renato zero';
+  document.getElementById('first_row_subtitle3').innerHTML='magari';
+  document.getElementById('first_row_image4').src='img/_italian_songs_4.jpg';
+  document.getElementById('first_row_title4').innerHTML='ornella vanoni';
+  document.getElementById('first_row_subtitle4').innerHTML='rossetto e cioccolato';
+  document.getElementById('first_row_image5').src='img/_italian_songs_5.jpg';
+  document.getElementById('first_row_title5').innerHTML='jovanotti';
+  document.getElementById('first_row_subtitle5').innerHTML='ti sposerò';
+  document.getElementById('first_row_image6').src='img/_italian_songs_6.jpg';
+  document.getElementById('first_row_title6').innerHTML='claudio baglioni';
+  document.getElementById('first_row_subtitle6').innerHTML='avrai';
 }
