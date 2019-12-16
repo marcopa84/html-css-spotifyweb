6e41ab8ee86329594237bf4d4fc3cd36e81188d6
Marco <marcopa84@hotmail.it>
2019-12-16 17:10:35 +0100

    First playlist DONE

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// // alert('hello!');
+// function myfunction(britney) {
+//   document.getElementById('title1').innerHTML='britney spears';
+//   document.getElementById('subtitle1').innerHTML='baby one more time';
+// }
+function myFunction() {
+  document.getElementById('playlist_title_first_row').innerHTML='the best of britney spears';
+  document.getElementById('first_row_image1').src='img/_britney1.jpg';
+  document.getElementById('first_row_title1').innerHTML='britney spears';
+  document.getElementById('first_row_subtitle1').innerHTML='baby one more time';
+  document.getElementById('first_row_image2').src='img/_britney2.jpg';
+  document.getElementById('first_row_title2').innerHTML='britney spears';
+  document.getElementById('first_row_subtitle2').innerHTML='circus';
+  document.getElementById('first_row_image3').src='img/_britney3.jpg';
+  document.getElementById('first_row_title3').innerHTML='britney spears';
+  document.getElementById('first_row_subtitle3').innerHTML='in the zone';
+  document.getElementById('first_row_image4').src='img/_britney4.png';
+  document.getElementById('first_row_title4').innerHTML='britney spears';
+  document.getElementById('first_row_subtitle4').innerHTML='toxic';
+  document.getElementById('first_row_image5').src='img/_britney5.jpg';
+  document.getElementById('first_row_title5').innerHTML='britney spears';
+  document.getElementById('first_row_subtitle5').innerHTML='glory';
+  document.getElementById('first_row_image6').src='img/_britney6.jpg';
+  document.getElementById('first_row_title6').innerHTML='britney spears';
+  document.getElementById('first_row_subtitle6').innerHTML='work bitch!';
+}
