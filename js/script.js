9449b8d56863076717dee932d76646752a7f1868
Marco <marcopa84@hotmail.it>
2020-01-31 09:52:14 +0100

    url bugs OK!

--- js/script.js
+++ js/script.js
@@ -3,41 +3,41 @@ function britney_spears() {
   document.getElementById('first_row_image1').src='img/britney1.jpg';
   document.getElementById('first_row_title1').innerHTML='britney spears';
   document.getElementById('first_row_subtitle1').innerHTML='baby one more time';
-  document.getElementById('first_row_image2').src='img/_britney2.jpg';
+  document.getElementById('first_row_image2').src='img/britney2.jpg';
   document.getElementById('first_row_title2').innerHTML='britney spears';
   document.getElementById('first_row_subtitle2').innerHTML='circus';
-  document.getElementById('first_row_image3').src='img/_britney3.jpg';
+  document.getElementById('first_row_image3').src='img/britney3.jpg';
   document.getElementById('first_row_title3').innerHTML='britney spears';
   document.getElementById('first_row_subtitle3').innerHTML='in the zone';
-  document.getElementById('first_row_image4').src='img/_britney4.png';
+  document.getElementById('first_row_image4').src='img/britney4.png';
   document.getElementById('first_row_title4').innerHTML='britney spears';
   document.getElementById('first_row_subtitle4').innerHTML='toxic';
-  document.getElementById('first_row_image5').src='img/_britney5.jpg';
+  document.getElementById('first_row_image5').src='img/britney5.jpg';
   document.getElementById('first_row_title5').innerHTML='britney spears';
   document.getElementById('first_row_subtitle5').innerHTML='glory';
-  document.getElementById('first_row_image6').src='img/_britney6.jpg';
+  document.getElementById('first_row_image6').src='img/britney6.jpg';
   document.getElementById('first_row_title6').innerHTML='britney spears';
   document.getElementById('first_row_subtitle6').innerHTML='work bitch!';
 }
 
 function italian_songs() {
   document.getElementById('playlist_title_first_row').innerHTML='the best of italian songs';
-  document.getElementById('first_row_image1').src='img/_italian_songs_1.jpg';
+  document.getElementById('first_row_image1').src='img/italian_songs_1.jpg';
   document.getElementById('first_row_title1').innerHTML='mina';
   document.getElementById('first_row_subtitle1').innerHTML='raso';
-  document.getElementById('first_row_image2').src='img/_italian_songs_2.jpg';
+  document.getElementById('first_row_image2').src='img/italian_songs_2.jpg';
   document.getElementById('first_row_title2').innerHTML='lucio battisti';
   document.getElementById('first_row_subtitle2').innerHTML='emozioni';
-  document.getElementById('first_row_image3').src='img/_italian_songs_3.jpg';
+  document.getElementById('first_row_image3').src='img/italian_songs_3.jpg';
   document.getElementById('first_row_title3').innerHTML='renato zero';
   document.getElementById('first_row_subtitle3').innerHTML='magari';
-  document.getElementById('first_row_image4').src='img/_italian_songs_4.jpg';
+  document.getElementById('first_row_image4').src='img/italian_songs_4.jpg';
   document.getElementById('first_row_title4').innerHTML='ornella vanoni';
   document.getElementById('first_row_subtitle4').innerHTML='rossetto e cioccolato';
-  document.getElementById('first_row_image5').src='img/_italian_songs_5.jpg';
+  document.getElementById('first_row_image5').src='img/italian_songs_5.jpg';
   document.getElementById('first_row_title5').innerHTML='jovanotti';
   document.getElementById('first_row_subtitle5').innerHTML='ti sposerò';
-  document.getElementById('first_row_image6').src='img/_italian_songs_6.jpg';
+  document.getElementById('first_row_image6').src='img/italian_songs_6.jpg';
   document.getElementById('first_row_title6').innerHTML='claudio baglioni';
   document.getElementById('first_row_subtitle6').innerHTML='avrai';
 }
